c13e9dae035d2d9626991f2b5ea5515e514cdb55
kpj <kim.philipp.jablonski@gmail.com>
2025-03-30 22:21:17 -0400

    Record Python dependencies

--- backend/routes/login_system.py
+++ backend/routes/login_system.py
@@ -3,9 +3,9 @@ from datetime import datetime, timedelta, timezone
 from typing import Annotated
 
 import jwt
+from jwt.exceptions import InvalidTokenError
 from fastapi import Depends, APIRouter, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jwt.exceptions import InvalidTokenError
 import bcrypt
 from pydantic import BaseModel
 
